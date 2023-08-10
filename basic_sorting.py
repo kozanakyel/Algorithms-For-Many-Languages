34836a3efee2df87b2e2bc3eb348d734383311ae
kozanakyel <kozanakyel@gmail.com>
2023-08-10 08:43:42 +0300

    addd binarysearch adnmergesort with test cases

--- basic_sorting.py
+++ basic_sorting.py
@@ -72,7 +72,72 @@ def merge_sort(arr):
 #             arr = self.random_array()
 #             sorted_arr = sorted(arr)
 #             merge_sort(arr)
-#             self.assertEqual(arr, sorted_arr)               
+#             self.assertEqual(arr, sorted_arr)   
+
+def merge(arr, l, m, r):
+    n1=m-l+1
+    n2=r-m
+    L=[0]*n1
+    R=[0]*n2
+    for i in range(0, n1):
+        L[i]=arr[l+i]
+    for j in range(0,n2):
+        R[j]=arr[m+1+j]
+    i=0
+    j=0
+    k=1
+    while i<n1 and j<n2:
+        if L[i] <= R[j]:
+            arr[k]=L[i]
+            i+=1
+        else:
+            arr[k]=R[j]
+            j+=1
+        k+=1
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
+        
+def merge_sort_v1(arr, l, r):
+    if l<r:
+        m=l+(r-l)//2
+        merge_sort_v1(arr,l,m)
+        merge_sort_v1(arr,m+1,r)
+        merge(arr,l,m,r)
+        
+def binary_search(a,v):
+    lt, rt = 0, len(a)-1
+    while lt <= rt:
+        mid = (lt+rt)//2
+        if a[mid]==v:
+            return mid
+        elif a[mid]<v:
+            lt=mid+1
+        else:
+            rt=mid-1
+    return None
+        
+
+class BinarySearchTestCase(unittest.TestCase):
+    def random_array(self):
+        return [random.randint(0, 100) for _ in range(random.randint(1, 100))]
+
+    def test_random(self):
+        for _ in range(10000):
+            a = sorted(self.random_array())
+            v = random.randint(0, 100)
+            ret = binary_search(a, v)
+            if ret is None:
+                for val in a:
+                    self.assertNotEqual(val, v)
+            else:
+                self.assertEqual(a[ret], v)
+                    
         
 class InsertionSortTestCase(unittest.TestCase):
     def random_array(self):
