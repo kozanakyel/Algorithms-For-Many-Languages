20d97a91e197ef24430e260114ec531a81095581
kozanakyel <kozanakyel@gmail.com>
2023-09-04 00:23:23 +0300

    edited new dynamic binary tree for java

--- SpaceComp.java
+++ SpaceComp.java
@@ -2,7 +2,8 @@ import java.util.ArrayList;
 
 public class SpaceComp {
 
-    static int counter = 0;   // Counter for recursive stack
+    static int counter = 0; // Counter for recursive stack
+
     public static void main(String[] args) {
         // SpaceComp spa = new SpaceComp();
 
@@ -40,43 +41,41 @@ public class SpaceComp {
         return a + b;
     }
 
-    static int f(int n){
+    static int f(int n) {
         counter++;
-        System.out.println("Call numbers: "+counter);
-        if (n<=1) {
+        System.out.println("Call numbers: " + counter);
+        if (n <= 1) {
             return 1;
         }
-        return f(n-1)+f(n-1);
+        return f(n - 1) + f(n - 1);
     }
 
-    static boolean isPrime(int n){
-        // O(sqrt(n))  Big O, time complexity
-        for(int x=2; x*x<=n; x++){
-            if(n%x==0){
+    static boolean isPrime(int n) {
+        // O(sqrt(n)) Big O, time complexity
+        for (int x = 2; x * x <= n; x++) {
+            if (n % x == 0) {
                 return false;
             }
         }
         return true;
     }
 
-    static void permutation(String str){
+    static void permutation(String str) {
         permutation(str, "");
     }
 
-
-    static void permutation(String str, String prefix){
-        if(str.length()==0){
+    // O(n*nlogn)
+    static void permutation(String str, String prefix) {
+        if (str.length() == 0) {
             System.out.println(prefix);
             counter++;
-        }else{
-            for(int i=0; i<str.length();i++){
-                String rem = str.substring(0, i) + str.substring(i+1);
-                permutation(rem, prefix+str.charAt(i));
+        } else {
+            for (int i = 0; i < str.length(); i++) {
+                String rem = str.substring(0, i) + str.substring(i + 1);
+                permutation(rem, prefix + str.charAt(i));
                 counter++;
             }
         }
     }
-
-    
 
 }
