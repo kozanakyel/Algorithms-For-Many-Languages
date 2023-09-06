21dfbde2cd07c441c82b8aa4d4494db11c6868cf
kozanakyel <kozanakyel@gmail.com>
2023-09-06 20:35:46 +0300

    edited permutation js file

---
+++ permutation.js
@@ -0,0 +1,17 @@
+function permutation(str) {
+    permutationHelper(str, "");
+}
+
+function permutationHelper(str, prefix) {
+    if (str.length === 0) {
+        console.log(prefix);
+    } else {
+        for (let i = 0; i < str.length; i++) {
+            const rem = str.substring(0, i) + str.substring(i + 1);
+            permutationHelper(rem, prefix + str.charAt(i));
+        }
+    }
+}
+
+// Example usage:
+permutation("abc");
